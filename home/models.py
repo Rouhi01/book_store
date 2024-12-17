ef97413cf60283161d4c219e9fa1ec681e2f9722
rouhi <r.shafahi11@gmail.com>
2024-12-17 11:22:07 +0330

    creating comment model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomments')
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    reply = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
+    is_reply = models.BooleanField(default=False)
+    body = models.TextField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_model_name(self):
+        return self.content_type.model_class().__name__
